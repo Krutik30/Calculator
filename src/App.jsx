6a3c089545dad54e7352abadc380182dc597402e
Krutik Aghera <agherakrutik99@gmail.com>
2023-01-29 22:45:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='grid'>
+      <button
+        className="jumbo"
+        id="clear"
+        value="AC"
+      >
+        AC
+      </button>
+      <button
+        id="divide"
+        value="/"
+      >
+        /
+      </button>
+      <button
+        id="multiply"
+        value="x"
+      >
+        x
+      </button>
+      <button id="seven"  value="7">
+        7
+      </button>
+      <button id="eight"  value="8">
+        8
+      </button>
+      <button id="nine"  value="9">
+        9
+      </button>
+      <button
+        id="subtract"
+        value="‑"
+      >
+        ‑
+      </button>
+      <button id="four"  value="4">
+        4
+      </button>
+      <button id="five"  value="5">
+        5
+      </button>
+      <button id="six" value="6">
+        6
+      </button>
+      <button
+        id="add"
+        value="+"
+      >
+        +
+      </button>
+      <button id="one"  value="1">
+        1
+      </button>
+      <button id="two"  value="2">
+        2
+      </button>
+      <button id="three" value="3">
+        3
+      </button>
+      <button
+        className="jumbo"
+        id="zero"
+        value="0"
+      >
+        0
+      </button>
+      <button id="decimal" value=".">
+        .
+      </button>
+      <button
+        id="equals"
+        value="="
+      >
+        =
+      </button>
+    </div>)
+}
+
+export default App
